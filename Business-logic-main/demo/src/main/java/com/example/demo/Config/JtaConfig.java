344bc6c0ec9df013f7de76b66671ef79fadab679
Icerzack <maxalkuz@gmail.com>
2023-04-26 18:18:31 +0300

    class fix

--- Business-logic-main/demo/src/main/java/com/example/demo/Config/JtaConfig.java
+++ Business-logic-main/demo/src/main/java/com/example/demo/Config/JtaConfig.java
@@ -1,71 +1,36 @@
 package com.example.demo.Config;
 
-import com.atomikos.icatch.jta.UserTransactionManager;
 import com.atomikos.icatch.jta.UserTransactionImp;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jta.atomikos.AtomikosDataSourceBean;
+import com.atomikos.icatch.jta.UserTransactionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 
-import javax.sql.DataSource;
-import javax.sql.XADataSource;
 import javax.transaction.SystemException;
-import java.util.Properties;
-
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
 
 @Configuration
-@EnableTransactionManagement
 public class JtaConfig {
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Autowired
-    private DataSource dataSource;
 
     @Bean(initMethod = "init", destroyMethod = "close")
-    public AtomikosDataSourceBean myDataSource() {
-
-        AtomikosDataSourceBean ds = new AtomikosDataSourceBean();
-        ds.setUniqueResourceName("postgres");
-        ds.setXaDataSource((XADataSource) dataSource);
-        ds.setXaDataSourceClassName("org.postgresql.xa.PGXADataSource");
-        Properties p = new Properties();
-        p.setProperty ( "user" , username );
-        p.setProperty ( "password" , password );
-        p.setProperty ( "serverName" , "localhost" );
-        p.setProperty ( "portNumber" , "5432" );
-        p.setProperty ( "databaseName" , "studs" );
-        ds.setXaProperties ( p );
-        ds.setMaxPoolSize(10);
-
-        return ds;
-    }
-
-    @Bean
-    public UserTransactionImp myTransactionImp() {
-        return new UserTransactionImp();
-    }
-
-    @Bean(initMethod = "init", destroyMethod = "close")
-    public UserTransactionManager userTransactionManager() throws SystemException {
+    public UserTransactionManager atomikosTransactionManager() {
         UserTransactionManager userTransactionManager = new UserTransactionManager();
-        userTransactionManager.setTransactionTimeout(300);
-        userTransactionManager.setForceShutdown(true);
+        userTransactionManager.setForceShutdown(false);
         return userTransactionManager;
     }
 
     @Bean
+    public UserTransaction atomikosUserTransaction() throws SystemException {
+        UserTransactionImp userTransaction = new UserTransactionImp();
+        userTransaction.setTransactionTimeout(10000);
+        return userTransaction;
+    }
+
+    @Bean
     public JtaTransactionManager transactionManager() throws SystemException {
-        JtaTransactionManager jtaTransactionManager = new JtaTransactionManager();
-        jtaTransactionManager.setTransactionManager(userTransactionManager());
-        jtaTransactionManager.setUserTransaction(userTransactionManager());
-        return jtaTransactionManager;
+        UserTransaction userTransaction = atomikosUserTransaction();
+        TransactionManager transactionManager = atomikosTransactionManager();
+        return new JtaTransactionManager(userTransaction, transactionManager);
     }
 }
